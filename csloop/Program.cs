ccbbc526071f7e4b1b366a6758f435e01a48c31d
Minji Kim <s2018s24@e-mirim.hs.kr>
2020-05-08 12:31:51 +0900

    움직이는 지렁이 코드

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace csloop
 {
@@ -28,6 +29,23 @@ namespace csloop
             foreach (var item in array)
             {
                 Console.WriteLine(item);
+            }
+
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if (x % 3 == 0)
+                    Console.WriteLine("__@");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^_@");
+
+                Thread.Sleep(100);
+                x++;
             }
         }
     }
