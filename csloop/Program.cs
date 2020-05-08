3fbba981ea68bcc43c14db2554a8118d7602d108
Minji Kim <s2018s24@e-mirim.hs.kr>
2020-05-08 12:06:47 +0900

    foreach 예제

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -18,6 +18,17 @@ namespace csloop
                 count++;
             }
             Console.WriteLine(count + "만큼 반복함");
+
+            String[] array = { "사과", "배", "포도", "딸기", "바나나" };
+            foreach(String item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
